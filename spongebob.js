892551199688f8848ca60590a1977b3ed7027e8b
Yeseong Jeon <cjhj131@gmail.com>
2023-01-21 00:12:53 -0800

    second version

---
+++ spongebob.js
@@ -0,0 +1,20 @@
+class Spongebob {
+    constructor(game) {
+        this.game = game;
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./sprites/spongebob.png"), 0, 0, 85, 200, 9, 0.035);
+                                                                //xStart, yStart, width, height, frameCount, frameDuration
+        this.x = 480;
+        this.y = 0;
+        this.speed = 100;
+
+    };
+
+    update(){
+        this.y += this.speed * this.game.clockTick;
+    };
+
+    draw(ctx) {
+        this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+       
+    };
+}
